3cbd2e5e67bda9b520b43706f487ab34a656a17c
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-28 20:54:05 +0530

    add CrossOrigin to fix CORS error

--- src/main/java/com/springboot/ipl_dashboard/controller/TeamController.java
+++ src/main/java/com/springboot/ipl_dashboard/controller/TeamController.java
@@ -5,7 +5,7 @@ import com.springboot.ipl_dashboard.model.Team;
 import com.springboot.ipl_dashboard.repository.MatchRepository;
 import com.springboot.ipl_dashboard.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class TeamController {
 
     private final TeamRepository teamRepository;
